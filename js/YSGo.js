f3ab9af3f7d8b9169df142bf153ea41348ee8b3b
NaiHeeeee <2794096231@qq.com>
2023-11-30 20:20:18 +0800

    Update

--- js/YSGo.js
+++ js/YSGo.js
@@ -2,10 +2,3 @@
 document.documentElement.addEventListener("click", function () {
     document.getElementById('YSGo').play();
 });
-
-// 重定向
-window.onload = function () {
-    setTimeout(function () {
-        window.location.href = 'https://www.zafu.edu.cn/';
-    }, 10000);
-};
